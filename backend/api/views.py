8eac6f1cf2e718b74c8ccffa6ed87c9cb173b3ca
sanimkhan <sanimkhan13@gmail.com>
2023-10-31 18:11:31 +0600

    Endpoint- /api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+
+
+def api_home(request):
+
+    try:
+        json_data = json.loads(request.body)
+    except json.JSONDecodeError as e:
+        return JsonResponse({'error': 'Invalid JSON data'}, status=400)
+    print(json_data)
+
+    return JsonResponse({"message": "Hi, this is django"})
