2d6a8aa9f999420b2a44083f9eca8419b4e4f360
sanimkhan <sanimkhan13@gmail.com>
2023-11-02 23:40:31 +0600

    Use- model_to_dict, @api_view in endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_home),
+    path('general-response', views.api_general_response),
+    path('django-response', views.api_django_response),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,23 @@
-import json
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 from django.forms.models import model_to_dict
 
 
-# from products.models import Product
-
-
-def api_home(request):
-
+def api_general_response(request):
     product = Product.objects.all().order_by("?").first()
     data = model_to_dict(product)
     # data = model_to_dict(product, fields= ['id'])
 
     return JsonResponse(data)
+
+
+@api_view(["GET"])
+def api_django_response(request):
+    product = Product.objects.all().order_by("?").first()
+    data = model_to_dict(product)
+    # data = model_to_dict(product, fields= ['id'])
+
+    return Response(data)
