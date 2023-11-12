86c9a69b63b1045dcb1a74278664fd46387e7312
sanimkhan <sanimkhan13@gmail.com>
2023-11-12 11:52:09 +0600

    Generic view CRUD

--- backend/products/migrations/views/generic_views.py
+++ backend/products/migrations/views/generic_views.py
@@ -3,11 +3,21 @@ from products.models import Product
 from products.serializers import PrimaryProductSerializer
 
 
-class BookDetailView(generics.RetrieveAPIView):
+# class ProductDetailView(generics.RetrieveAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = PrimaryProductSerializer
+#
+#
+# class ProductListView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = PrimaryProductSerializer
+
+
+class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = PrimaryProductSerializer
 
 
-class BookListView(generics.ListAPIView):
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = PrimaryProductSerializer
--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from products.migrations.views.generic_views import BookDetailView, BookListView
+from products.migrations.views.generic_views import ProductDetail, ProductList
 from products.migrations.views.views import product_detail
 
 urlpatterns = [
     path('<int:pk>', product_detail),
-    path('generics/<int:pk>/', BookDetailView.as_view()),
-    path('generics/', BookListView.as_view()),
+    path('generics/<int:pk>/', ProductDetail.as_view()),
+    path('generics/', ProductList.as_view()),
 ]
